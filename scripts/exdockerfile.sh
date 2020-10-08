4bb1351f1f12c0d40f3114e12cf47203ff38f365
Altair Wei <altair_wei@outlook.com>
2020-10-08 19:36:33 +0800

    delete tar junk file

--- scripts/exdockerfile.sh
+++ scripts/exdockerfile.sh
@@ -2,6 +2,7 @@
 
 set -e -o pipefail
 shopt -s failglob
+shopt -s globstar
 export LC_ALL=C
 
 IMAGE=$1
@@ -9,3 +10,8 @@ LAYER_HASH=$2
 TARGET=$3
 
 tar --to-command="tar -xf - $TARGET" -xf $IMAGE $LAYER_HASH/layer.tar
+
+find ${TARGET} -name .wh..wh..opq | while read file; do
+    rm "$file"
+    echo "Found and deleted junk file: $file"
+done
