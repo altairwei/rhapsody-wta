f89f7ece3cefa3a87034edcd12b515a4589eb1ce
Altair Wei <altair_wei@outlook.com>
2020-10-10 17:21:36 +0800

    add script to build genome indexes

---
+++ scripts/build_genome_index.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#PBS -l mem=96gb,nodes=1:ppn=16,walltime=48:00:00
+#PBS -q fat
+#PBS -N genome-index
+#PBS -o logs
+#PBS -e logs
+
+set -e -o pipefail
+shopt -s failglob
+export LC_ALL=C
+
+cd $PBS_O_WORKDIR
+
+source activate wta
+
+GTF_FILES="$1"
+shift
+FASTA_FILES="$@"
+
+filename=$(basename -- "$GTF_FILES")
+extension="${GTF_FILES##*.}"
+filename="${filename%.*}"
+
+mkdir -p data/reference_indexes
+STAR \
+    --runThreadN 16 \
+    --runMode genomeGenerate \
+    --genomeDir data/reference_indexes \
+    --genomeFastaFiles "$FASTA_FILES" \
+    --sjdbGTFfile "$GTF_FILES"
+
+tar --transform "s#^\.#${filename}#" \
+    -cvzf data/${filename}.tar.gz -C data/reference_indexes .
