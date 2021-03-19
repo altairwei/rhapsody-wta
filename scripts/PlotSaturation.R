486befcf6b709f6ff2bf9f097d84afc2051c6394
Altair Wei <altair_wei@outlook.com>
2021-03-19 10:29:27 +0800

    PlotSaturation.R supports multiple inputs

--- scripts/PlotSaturation.R
+++ scripts/PlotSaturation.R
@@ -10,32 +10,91 @@ require_dependencies <- function(x) {
 }
 
 require_dependencies(c(
+  "optparse",
   "readr",
+  "dplyr",
   "ggplot2",
   "magrittr",
   "cowplot"
 ))
 
 library(magrittr)
+library(optparse)
 
 ggplot2::theme_set(cowplot::theme_cowplot())
 
-args <- commandArgs(trailingOnly = TRUE)
+parser <- OptionParser()
 
-if (length(args) < 1) {
-    stop("ERROR: Image filename is required.")
+parser <- add_option(parser,
+  c("-o", "--output-image"),
+  dest = "output_image",
+  action = "store",
+  default = "saturation_curve.png",
+  type = "character",
+  help = paste0("Output image file. [default: %default]"))
+parser <- add_option(parser,
+  c("-w", "--which-name"),
+  dest = "which_name",
+  action = "store",
+  default = 0,
+  type = "integer",
+  help = paste0(
+    "Which part of input file path should be assigned to ",
+    "the group name of data frame. [default: %default]"))
+
+arguments <- parse_args2(parser)
+options <- arguments$options
+options$positionals <- arguments$args
+
+if (length(options$positionals) < 1) {
+  options$stdin <- TRUE
+} else {
+  options$stdin <- FALSE
 }
 
-filename <- args[1]
+filename <- options$output_image
 
-stdin_conn <- file("stdin")
-df <- readr::read_csv(paste(collapse = "\n", readLines(stdin_conn)))
-close(stdin_conn)
+if (length(options$positionals) < 2) {
+  # Plot single data frame.
+  if (length(options$positionals) < 1) {
+    stdin_conn <- file("stdin")
+    df <- readr::read_csv(paste(collapse = "\n", readLines(stdin_conn)))
+    close(stdin_conn)
+  } else {
+    input_file <- options$positionals[1]
+    df <- readr::read_csv(input_file)
+  }
 
-p <- ggplot2::ggplot(df, ggplot2::aes(
-    depths, detected_genes, color = origin)) +
-  ggplot2::geom_point() +
-  ggplot2::geom_line()
+  p <- ggplot2::ggplot(df, ggplot2::aes(
+      depths, detected_genes, color = origin)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_line()
+} else {
+  # Plot multiple data frame in one image.
+  input_files <- options$positionals
+  df_names <- sapply(input_files, function(x) {
+    path_parts <- strsplit(x, .Platform$file.sep)[[1]]
+    if (options$which_name == 0 ||
+          options$which_name > length(path_parts)) {
+      x
+    } else {
+      path_parts[options$which_name]
+    }
+  }, USE.NAMES = FALSE)
+
+  names(input_files) <- df_names
+
+  df_list <- lapply(seq_along(input_files), function(index) {
+    input_df <- readr::read_csv(input_files[[index]])
+    dplyr::mutate(input_df, sample = names(input_files)[[index]])
+  })
+
+  df <- dplyr::bind_rows(df_list)
+
+  p <- ggplot2::ggplot(df, ggplot2::aes(
+      depths, detected_genes, alpha = origin, color = sample)) +
+    ggplot2::geom_line()
+}
 
 
 ggplot2::ggsave(filename, p, device = "png")
