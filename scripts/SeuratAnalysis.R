68841cba7d750c8c310aa9155aacc96d9187fac4
Altair Wei <altair_wei@outlook.com>
2021-11-17 09:17:44 +0800

    add directly Seurat object merge action

--- scripts/SeuratAnalysis.R
+++ scripts/SeuratAnalysis.R
@@ -6,6 +6,26 @@ ggplot2::theme_set(cowplot::theme_cowplot())
 
 parser <- OptionParser()
 parser <- add_option(parser,
+  c("--analysis"),
+  dest = "analysis",
+  action = "store",
+  default = "standalone",
+  help = paste0("Which analysis should be applied to samples.",
+    "[default: %default]"))
+parser <- add_option(parser,
+  c("-i", "--integrate"),
+  dest = "analysis",
+  action = "callback",
+  callback = function(...) "integrate",
+  help = paste0("Integrate data by using Seurat."))
+parser <- add_option(parser,
+  c("--merge"),
+  dest = "analysis",
+  action = "callback",
+  callback = function(...) "merge",
+  help = paste0("Merge all samples into one object without batch effect ",
+    "correction."))
+parser <- add_option(parser,
   c("-d", "--draw-plot"),
   dest = "draw_plot",
   action = "store_true",
@@ -72,13 +92,6 @@ parser <- add_option(parser,
   type = "character",
   help = paste0("Mitochondrial gene list. [default: %default]"))
 parser <- add_option(parser,
-  c("-i", "--integrate"),
-  dest = "integrate",
-  action = "store_true",
-  default = FALSE,
-  type = "logical",
-  help = paste0("Integrate data by useing Seurat. [default: %default]"))
-parser <- add_option(parser,
   c("--group-rule"),
   dest = "group_rule",
   action = "store",
@@ -102,7 +115,8 @@ parser <- add_option(parser,
   action = "store",
   default = 5,
   type = "integer",
-  help = paste0("How many neighbors to use when picking anchors. [default: %default]")
+  help = paste0("How many neighbors to use when picking anchors.",
+    " [default: %default]")
 )
 parser <- add_option(parser,
   c("-r", "--reduction"),
@@ -110,7 +124,8 @@ parser <- add_option(parser,
   action = "store",
   default = "cca",
   type = "character",
-  help = paste0("Dimensional reduction to perform when finding anchors. [default: %default]")
+  help = paste0("Dimensional reduction to perform when finding anchors.",
+    " [default: %default]")
 )
 parser <- add_option(parser,
   c("--process"),
@@ -192,11 +207,73 @@ if (options$process > 1) {
   options(future.globals.maxSize = options$memory * 1024^3)
 }
 
-if (isTRUE(options$integrate)) {
-  obj_combined <- NULL
-  if (!is.null(options$use_cache)) {
-    obj_combined <- readRDS(options$use_cache)
-  } else {
+run_analysis <- switch(options$analysis,
+
+  integrate = function() {
+    #------------------------------------------------------------
+    # Integrate all sample into one object.
+    #------------------------------------------------------------
+
+    obj_combined <- NULL
+    if (!is.null(options$use_cache)) {
+      obj_combined <- readRDS(options$use_cache)
+    } else {
+      # Convert raw data into Seurat object for each sample.
+      obj_list <- lapply(options$data_folders, function(base_dir) {
+        expr_matrix <- rhapsodykit::read_rhapsody_wta(base_dir, options$use_mtx)
+        seurat_obj <- Seurat::CreateSeuratObject(
+          counts = expr_matrix, project = basename(base_dir))
+        seurat_obj$sample <- basename(base_dir)
+
+        if (!is.null(options$group_rule)) {
+          seurat_obj$group <- stringr::str_extract(
+            basename(base_dir), options$group_rule)
+        } else {
+          seurat_obj$group <- seurat_obj$sample
+        }
+
+        seurat_obj
+      })
+
+      # Integrate all samples.
+      obj_combined <- rhapsodykit::integrated_sample_analysis(
+        obj_list, reduction = options$reduction, k.anchor = options$anchors,
+        reference = options$reference
+      )
+    }
+
+    output_folder <- options$output_folder
+    if (!dir.exists(output_folder)) {
+      dir.create(output_folder)
+    }
+
+    if (isTRUE(options$diff_gene)) {
+      #FIXME: Does this make sense?
+      rhapsodykit::perform_find_all_markers(obj_combined, output_folder)
+
+      # Conserved markers across conditions
+      markers_conserved_df <- rhapsodykit::find_all_conserved_markers(
+        obj_combined)
+      readr::write_csv(
+        markers_conserved_df, file.path(output_folder, "Markers_Conserved.csv"))
+
+      # Identify differential expressed genes across conditions
+      rhapsodykit::perform_diff_gene(
+        obj_combined, output_folder, options$draw_plot)
+    }
+
+    if (isTRUE(options$produce_cache)) {
+      saveRDS(obj_combined,
+        file = file.path(output_folder, "Seurat_Object_Combined.rds"))
+    }
+
+    if (isTRUE(options$draw_plot)) {
+      rhapsodykit::generate_seurat_plots(obj_combined, output_folder)
+    }
+  },
+
+  merge = function() {
+    # Convert raw data into Seurat object for each sample.
     obj_list <- lapply(options$data_folders, function(base_dir) {
       expr_matrix <- rhapsodykit::read_rhapsody_wta(base_dir, options$use_mtx)
       seurat_obj <- Seurat::CreateSeuratObject(
@@ -213,85 +290,71 @@ if (isTRUE(options$integrate)) {
       seurat_obj
     })
 
-    obj_combined <- rhapsodykit::integrated_sample_analysis(
-      obj_list, reduction = options$reduction, k.anchor = options$anchors,
-      reference = options$reference
+    output_folder <- options$output_folder
+    if (!dir.exists(output_folder)) {
+      dir.create(output_folder)
+    }
+
+    obj_merged <- rhapsodykit::merge_sample(obj_list)
+
+    if (isTRUE(options$produce_cache)) {
+      saveRDS(obj_merged,
+        file = file.path(output_folder, "Seurat_Object_Merged.rds"))
+    }
+  },
+
+  standalone = function() {
+    #------------------------------------------------------------
+    # Process all samples in parallel.
+    #------------------------------------------------------------
+
+    invisible(
+      lapply(options$data_folders, function(base_dir) {
+        expr_matrix <- rhapsodykit::read_rhapsody_wta(base_dir, options$use_mtx)
+
+        if (isTRUE(options$compress) && !isTRUE(options$use_mtx)) {
+          input_file <- Sys.glob(
+            file.path(base_dir, "*_RSEC_MolsPerCell.csv"))
+          mtx_file <- sub(
+            "_RSEC_MolsPerCell.csv$",
+            "_Expression_Matrix.mtx", input_file)
+          colnames_file <- sprintf("%s.colnames", mtx_file)
+          rownames_file <- sprintf("%s.rownames", mtx_file)
+
+          message(sprintf("Writting %s", mtx_file))
+          Matrix::writeMM(expr_matrix, file = mtx_file)
+          write(colnames(expr_matrix), colnames_file)
+          write(rownames(expr_matrix), rownames_file)
+        }
+
+        output_folder <- file.path(base_dir, "SeuratAnalysis")
+
+        if (!dir.exists(output_folder)) {
+          dir.create(output_folder)
+        }
+
+        seurat_obj <- Seurat::CreateSeuratObject(
+          counts = expr_matrix, project = basename(base_dir))
+
+        seurat_obj <- rhapsodykit::single_sample_analysis(
+          seurat_obj, options$mt_gene_file, options$cp_gene_file)
+
+        if (isTRUE(options$diff_gene)) {
+          rhapsodykit::perform_find_all_markers(seurat_obj, output_folder)
+        }
+
+        if (isTRUE(options$produce_cache)) {
+          saveRDS(seurat_obj,
+            file = file.path(output_folder, "Seurat_Object.rds"))
+        }
+
+        if (isTRUE(options$draw_plot)) {
+          rhapsodykit::generate_seurat_plots(seurat_obj, output_folder)
+        }
+
+      })
     )
   }
+)
 
-  output_folder <- options$output_folder
-  if (!dir.exists(output_folder)) {
-    dir.create(output_folder)
-  }
-
-  if (isTRUE(options$diff_gene)) {
-    #FIXME: Does this make sense?
-    rhapsodykit::perform_find_all_markers(obj_combined, output_folder)
-
-    # Conserved markers across conditions
-    markers_conserved_df <- rhapsodykit::find_all_conserved_markers(
-      obj_combined)
-    readr::write_csv(
-      markers_conserved_df, file.path(output_folder, "Markers_Conserved.csv"))
-
-    # Identify differential expressed genes across conditions
-    rhapsodykit::perform_diff_gene(
-      obj_combined, output_folder, options$draw_plot)
-  }
-
-  if (isTRUE(options$produce_cache)) {
-    saveRDS(obj_combined,
-      file = file.path(output_folder, "Seurat_Object_Combined.rds"))
-  }
-
-  if (isTRUE(options$draw_plot)) {
-    rhapsodykit::generate_seurat_plots(obj_combined, output_folder)
-  }
-} else {
-  invisible(
-    lapply(options$data_folders, function(base_dir) {
-      expr_matrix <- rhapsodykit::read_rhapsody_wta(base_dir, options$use_mtx)
-
-      if (isTRUE(options$compress) && !isTRUE(options$use_mtx)) {
-        input_file <- Sys.glob(
-          file.path(base_dir, "*_RSEC_MolsPerCell.csv"))
-        mtx_file <- sub(
-          "_RSEC_MolsPerCell.csv$",
-          "_Expression_Matrix.mtx", input_file)
-        colnames_file <- sprintf("%s.colnames", mtx_file)
-        rownames_file <- sprintf("%s.rownames", mtx_file)
-
-        message(sprintf("Writting %s", mtx_file))
-        Matrix::writeMM(expr_matrix, file = mtx_file)
-        write(colnames(expr_matrix), colnames_file)
-        write(rownames(expr_matrix), rownames_file)
-      }
-
-      output_folder <- file.path(base_dir, "SeuratAnalysis")
-
-      if (!dir.exists(output_folder)) {
-        dir.create(output_folder)
-      }
-
-      seurat_obj <- Seurat::CreateSeuratObject(
-        counts = expr_matrix, project = basename(base_dir))
-
-      seurat_obj <- rhapsodykit::single_sample_analysis(
-        seurat_obj, options$mt_gene_file, options$cp_gene_file)
-
-      if (isTRUE(options$diff_gene)) {
-        rhapsodykit::perform_find_all_markers(seurat_obj, output_folder)
-      }
-
-      if (isTRUE(options$produce_cache)) {
-        saveRDS(seurat_obj,
-          file = file.path(output_folder, "Seurat_Object.rds"))
-      }
-
-      if (isTRUE(options$draw_plot)) {
-        rhapsodykit::generate_seurat_plots(seurat_obj, output_folder)
-      }
-
-    })
-  )
-}
+run_analysis()
