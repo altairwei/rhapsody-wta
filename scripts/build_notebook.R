64e696b145d181a8ef3f005b98983d04830dbe16
Altair Wei <altair_wei@outlook.com>
2021-11-28 11:45:59 +0800

    add differential abundance analysis

--- scripts/build_notebook.R
+++ scripts/build_notebook.R
@@ -2,11 +2,12 @@
 
 workflowr::wflow_build(
     c(
+        "analysis/sample_integration.Rmd",
         "analysis/cell_annotation.Rmd",
         "analysis/differential_state_analysis.Rmd",
         "analysis/about.Rmd",
         "analysis/license.Rmd",
         "analysis/index.Rmd"
     ),
-    make = TRUE
+    make = FALSE
 )
